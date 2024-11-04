de9a6bc9e2f1130c8ad2f0dffdadbc5642173ccc
papasim824 <papasim824@gmail.com>
2024-11-04 20:59:00 +0900

    mod

--- static/app.js
+++ static/app.js
@@ -5,22 +5,41 @@ const gameSites = [
 
 function loadGameSites() {
     const container = document.getElementById("game-list");
+    container.innerHTML = '';
 
     for (const url of gameSites) {
         const iconUrl = `${url[0].replace('/index.html', '')}/static/icon.png`;
         const name = url[1];
 
-        if (iconUrl) {
-            const linkElement = document.createElement("a");
-            linkElement.href = url[0];
-            linkElement.classList.add("game-link");
-            linkElement.innerHTML = `
+        const linkElement = document.createElement("a");
+        linkElement.href = "#";
+        linkElement.classList.add("game-link");
+        linkElement.innerHTML = `
           <img src="${iconUrl}" alt="${name} icon" />
           <span>${name}</span>
         `;
-            container.appendChild(linkElement);
-        }
+
+        linkElement.addEventListener('click', (e) => {
+            e.preventDefault();
+            loadGame(url[0]);
+        });
+
+        container.appendChild(linkElement);
     }
+}
+
+function loadGame(gameUrl) {
+    const container = document.getElementById("game-list");
+    container.innerHTML = `
+        <div class="game-container">
+            <button id="back-button">← 戻る</button>
+            <iframe src="${gameUrl}" frameborder="0" width="100%" height="400px"></iframe>
+        </div>
+    `;
+
+    document.getElementById("back-button").addEventListener('click', () => {
+        loadGameSites();
+    });
 }
 
 loadGameSites();
