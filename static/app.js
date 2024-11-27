3e8faaa4088111fdd74eb0b6755e94bc1640a461
papasim824 <papasim824@gmail.com>
2024-11-27 21:07:45 +0900

    mod

--- static/app.js
+++ static/app.js
@@ -4,7 +4,7 @@ const gameSites = [
     ["./apps/math/index.html", "たしざん"],
     ["./apps/fireworks/index.html", "はなび"],
     ["./apps/car/index.html", "くるま"],
-    ["./apps/pict_roulette/index.html", "るーれっと"]
+    ["./apps/pict_roulette/index.html", "るーれっと"],
     ["https://qiita.com/papasim824/items/9c8f7f84c04c9b37432c", "えほん"]
 ];
 
